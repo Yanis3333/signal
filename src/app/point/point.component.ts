99708104ef1be91055dacb00242d3e57710993d0
Yanis3333 <yanisdezzaz33@gmail.com>
2024-05-13 12:21:16 +0200

    Probléme supérieur signal

--- src/app/point/point.component.ts
+++ src/app/point/point.component.ts
@@ -8,11 +8,13 @@ import { Component, WritableSignal, signal } from '@angular/core';
   styleUrl: './point.component.scss'
 })
 export class PointComponent {
+  i!: number;
   indexes:number[] = [0,1,2,3,4];
   emptyPoint:string = '/src/assets/rond.png'
   fullPoint:string = '/src/assets/rond_plein.png'
 
-  etat = false
-
   quantity:WritableSignal<number> = signal(2);
+
+
+
 }
