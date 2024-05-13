515f3904bae03221a3ce892a21b9e42961dec0a7
Yanis3333 <yanisdezzaz33@gmail.com>
2024-05-13 11:55:39 +0200

    début for

---
+++ src/app/point/point.component.ts
@@ -0,0 +1,18 @@
+import { Component, WritableSignal, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-point',
+  standalone: true,
+  imports: [],
+  templateUrl: './point.component.html',
+  styleUrl: './point.component.scss'
+})
+export class PointComponent {
+  indexes:number[] = [0,1,2,3,4];
+  emptyPoint:string = '/src/assets/rond.png'
+  fullPoint:string = '/src/assets/rond_plein.png'
+
+  etat = false
+
+  quantity:WritableSignal<number> = signal(2);
+}
